d31dcf73fa92e34334ef92359f23f8130382fc69
sw192 <sw192@gmail.com>
2026-06-16 09:42:30 +0530

    migrations are done

---
+++ SLMS.DAL/Migrations/20260615123325_AddDigitalLibraryModule.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SLMS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDigitalLibraryModule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DownloadHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EmployeeId = table.Column<int>(type: "integer", nullable: false),
+                    DigitalContentId = table.Column<int>(type: "integer", nullable: false),
+                    DownloadedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DownloadHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DownloadHistory_DigitalContents_DigitalContentId",
+                        column: x => x.DigitalContentId,
+                        principalTable: "DigitalContents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DownloadHistory_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DownloadHistory_DigitalContentId",
+                table: "DownloadHistory",
+                column: "DigitalContentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DownloadHistory_EmployeeId",
+                table: "DownloadHistory",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DownloadHistory");
+        }
+    }
+}
---
+++ SLMS.DOL/Entities/DownloadHistory.cs
@@ -0,0 +1,17 @@
+﻿using SLMS.DOL.Common;
+
+namespace SLMS.DOL.Entities;
+
+public class DownloadHistory : BaseEntity
+{
+    public int EmployeeId { get; set; }
+
+    public Employee Employee { get; set; } = null!;
+
+    public int DigitalContentId { get; set; }
+
+    public DigitalContent DigitalContent { get; set; } = null!;
+
+    public DateTime DownloadedOn { get; set; }
+        = DateTime.UtcNow;
+}
