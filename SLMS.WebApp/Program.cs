3427bfb48ab277a17e67a63b60e3aea3e4c3deaf
Administrator <Administrator@SW037>
2026-06-18 12:22:10 +0530

    Merged inventory module into development

--- SLMS.WebApp/Program.cs
+++ SLMS.WebApp/Program.cs
@@ -1,48 +1,44 @@
 using SLMS.WebApp.Services;
-<<<<<<< HEAD
-=======
 using SLMS.WebApp.Services.Interfaces;
->>>>>>> feature-inventory
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-<<<<<<< HEAD
+
+builder.Services.AddHttpClient();
+
 builder.Services.AddHttpClient<EmployeeService>(
     client =>
     {
         client.BaseAddress =
             new Uri("https://localhost:7277/");
     });
+
 builder.Services.AddHttpClient<CustodyHistoryService>(
     client =>
     {
         client.BaseAddress =
             new Uri("https://localhost:7277/");
     });
+
 builder.Services.AddHttpClient<AuditLogService>(
     client =>
     {
         client.BaseAddress =
             new Uri("https://localhost:7277/");
     });
-=======
-
-builder.Services.AddHttpClient();
 
 builder.Services.AddScoped<
     IInventoryService,
     InventoryService>();
 
->>>>>>> feature-inventory
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
